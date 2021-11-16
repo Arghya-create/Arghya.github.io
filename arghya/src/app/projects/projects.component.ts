11ac3928bb2570912a96c375c26673e359f7fd29
git_arghya <invinciblearghya@gmail.com>
2021-11-17 04:03:36 +0530

    project half commit

--- arghya/src/app/projects/projects.component.ts
+++ arghya/src/app/projects/projects.component.ts
@@ -9,7 +9,12 @@ export class ProjectsComponent implements OnInit {
 
   constructor() { }
 
+  project: string = '';
   ngOnInit(): void {
+  }
+  viewDetails(val: string) {
+    console.log("value",val);
+    this.project = val;
   }
 
 }
